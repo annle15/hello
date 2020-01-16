e160ef494582c3091e6bf76494088a75858b4ce0
annle15 <59975309+annle15@users.noreply.github.com>
2020-01-16 12:50:43 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* "Hello World", My first program
+Name: Ann Le
+Date: 2020/01/14
+*/
+#include <stdio.h>
+
+int main(void) {
+  printf("Ann Le\n");
+  printf("Undeclared"\n);
+  return 0;
+}
+
+// Executable code
+
+//left of main: it returns that type of value
